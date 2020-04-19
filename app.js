f4c061b82760f8aa8df0865c4a0e74b25416adda
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-04-19 20:24:25 +0530

    create passport for authentication

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
-// Connect to Databse
+// Connect To Database
 mongoose.connect(config.database);
 
 // On Connection
 mongoose.connection.on('connected', () => {
-    console.log('Connected to database : '+config.database);
+  console.log('Connected to database '+config.database);
 });
 
-// Database Error
+// On Error
 mongoose.connection.on('error', (err) => {
-    console.log('Database error : '+err);
+  console.log('Database error: '+err);
 });
 
 const app = express();
@@ -32,17 +32,23 @@ app.use(cors());
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Body parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 
-app.use('/users',users);
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
 
 // Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid Endpoint');
+  res.send('Invalid Endpoint');
 });
 
 // Start Server
 app.listen(port, () => {
-    console.log(`Server started on port : ${port}`);
-})
+  console.log('Server started on port '+port);
+});
