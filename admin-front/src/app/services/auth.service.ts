ed35ea9cf02289219d62e23aa47f5bf162904075
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-04-20 02:56:24 +0530

    link dashboard and profile router to navbar and show user login details on pfofile page

--- admin-front/src/app/services/auth.service.ts
+++ admin-front/src/app/services/auth.service.ts
@@ -25,6 +25,19 @@ export class AuthService {
 
   }
 
+  getProfile(){
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type','application/json');
+    return this.http.get('http://localhost:3000/users/profile',{headers: headers}).map(res => res.json());
+  }
+
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
   storeUserData(token, user){
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
